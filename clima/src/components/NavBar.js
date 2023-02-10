bbf89fd9ac16def75e5f4c6a64ffad3cbe0bdc72
sebaroi <sroizenberg@gmail.com>
2023-02-10 15:57:38 -0300

    ND-1_add_react

---
+++ clima/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='container-fluid'>
+            <h3 className="mx-auto">Prediccion Meteorologica</h3>
+        </div>
+    );
+}
+
+export default NavBar;
